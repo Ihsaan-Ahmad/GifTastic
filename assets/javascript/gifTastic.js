4523f2dae5e8a2a7359921babf712bbb1ca4194b
Ihsaan Ahmad <ihsaan.edu4321@gmail.com>
2019-10-02 23:46:16 -0500

    fix bug

--- assets/javascript/gifTastic.js
+++ assets/javascript/gifTastic.js
@@ -1,42 +1,78 @@
-$(document).ready(function() {
-  /*global variables
-------------------------------------------------------*/
+var topics = ["anime", "games", "coding", "coffee"];
 
-  var $input = $("#input");
-  var $submit = $("#submit");
-  var $imgBody = $("#img-body");
-  var apiKey = "5TQIfDl6GAYoR6ZkWZa5yZYpJ8LHTeGC";
-
-  //Get value when user presses submit
-
-  $submit.on("click", function(event) {
-    event.preventDefault();
-    $imgBody.empty();
-    var inputVal = $input.val();
-    getGifTastic(inputVal);
-  });
-
-  //Make a request to the giphy api witht he input value
-
-  function getGifTastic(inputVal) {
-    $.get(
-      "http://api.giphy.com/v1/gifs/search?q=" +
-        inputVal +
-        "&api_key=" +
-        apiKey +
-        "&limit=5%22"
-    ).done(function(data) {
-      for (var i = 0; i < 5; i++) {
-        var gifImg = data.data[i].images.downsized.url;
-        createBox(gifImg);
-      }
-    });
+function renderButtons() {
+  $("#addButton").empty();
+  //for loop that loops through array and creates button
+  for (var i = 0; i < topics.length; i++) {
+    var button = $("<button>");
+    button.addClass("topic");
+    button.attr("data-name", topics[i]);
+    button.text(topics[i]);
+    $("#addButton").append(button);
   }
+  addGif();
+}
 
-  function createBox(gifImg) {
-    var $newImg = $("<img>");
-    $newImg.attr("src", gifImg);
-    $newImg.addClass("img-box");
-    $imgBody.append($newImg);
+$("#addGif").on("click", function() {
+  var userInput = $("#gif-input")
+    .val()
+    .trim();
+  console.log($("#gif-input"));
+  topics.push(userInput);
+  renderButtons();
+  return false;
+
+  if (userInput === " ") {
+    $("#gif-input")
+      .val()
+      .trim();
   }
 });
+
+renderButtons();
+
+function addGif() {
+  $("button").on("click", function() {
+    var p = $(this).data("name");
+    var queryURL =
+      "http://api.giphy.com/v1/gifs/search?q=" +
+      p +
+      "&api_key=5TQIfDl6GAYoR6ZkWZa5yZYpJ8LHTeGC";
+
+    $.ajax({ url: queryURL, method: "GET" }).done(function(response) {
+      var results = response.data;
+      console.log(response);
+
+      for (var i = 0; i < results.length; i++) {
+        var gifDiv = $('<div class="item">');
+        var rating = results[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+
+        var giphyImg = $("<img>");
+        giphyImg.attr("src", results[i].images.fixed_height_still.url);
+        giphyImg.attr("data-still", results[i].images.fixed_height_still.url);
+        giphyImg.attr("data-animate", results[i].images.fixed_height.url);
+        giphyImg.attr("data-state", results[i].images.fixed_height_still.url);
+
+        gifDiv.append(giphyImg);
+        gifDiv.append(p);
+
+        $("#gifsAppearHere").prepend(gifDiv);
+      }
+
+      $(".item")
+        .children("img")
+        .on("click", function() {
+          var state = $(this).attr("data-state");
+
+          if (state == "still") {
+            $(this).attr("src", $(this).data("animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).data("still"));
+            $(this).attr("data-state", "still");
+          }
+        });
+    });
+  });
+}
